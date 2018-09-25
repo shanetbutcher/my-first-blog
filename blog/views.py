28598d8d15475a8d12b9bc17fa7b3c28d9c31bd1
Shane <dayenlight@hotmail.com>
2018-09-24 20:13:19 -0400

    changed password

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {"posts": posts})
+	
+def about(request):
+    about = Post.objects.filter(title__contains='About')
+    return render(request, 'blog/about.html', {'about':about})
